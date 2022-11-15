8f4a10db0032a83d7c409befaa2a6dd364b20a0a
Ning Tang <89922630+tangn121@users.noreply.github.com>
2022-11-14 21:14:10 -0600

    Update get_data.sh

--- batch layer/get_data.sh
+++ batch layer/get_data.sh
@@ -6,9 +6,13 @@ curl https://datasets.imdbws.com/title.crew.tsv.gz | hdfs dfs -put - /tmp/tangn/
 
 curl https://datasets.imdbws.com/title.ratings.tsv.gz | hdfs dfs -put - /tmp/tangn/movie/data/rating.tsv.gz
 
+curl https://datasets.imdbws.com/name.basics.tsv.gz | hdfs dfs -put - /tmp/tangn/movie/data/name.tsv.gz
+
 #unzip file
 hdfs dfs -cat /tmp/tangn/movie/data/title.tsv.gz | gunzip | hdfs dfs -put - /tmp/tangn/movie/data/title/title.tsv
 
 hdfs dfs -cat /tmp/tangn/movie/data/crew.tsv.gz | gunzip | hdfs dfs -put - /tmp/tangn/movie/data/crew/crew.tsv
 
 hdfs dfs -cat /tmp/tangn/movie/data/rating.tsv.gz | gunzip | hdfs dfs -put - /tmp/tangn/movie/data/rating/rating.tsv
+
+hdfs dfs -cat /tmp/tangn/movie/data/name.tsv.gz | gunzip | hdfs dfs -put - /tmp/tangn/movie/data/name/name.tsv
