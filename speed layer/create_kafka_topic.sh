f8205025ad84dc0363d1ae27317743a5e72c1e79
Ning Tang <89922630+tangn121@users.noreply.github.com>
2022-11-30 17:56:35 -0600

    Update create_kafka_topic.sh

--- speed layer/create_kafka_topic.sh
+++ speed layer/create_kafka_topic.sh
@@ -1,5 +1,5 @@
 $ cd kafka_2.12-2.6.2/bin
 
 # create a topic
-$ kafka-topics.sh --create --zookeeper z-2.mpcs530142022.7vr20l.c19.kafka.us-east-1.amazonaws.com:2181,z-1.mpcs530142022.7vr20l.c19.kafka.us-east-1.amazonaws.com:2181,z-3.mpcs530142022.7vr20l.c19.kafka.us-east-1.amazonaws.com:2181 --replication-factor 1 --partitions 1 --topic tangn-pulic-rating
+$ kafka-topics.sh --create --zookeeper z-2.mpcs530142022.7vr20l.c19.kafka.us-east-1.amazonaws.com:2181,z-1.mpcs530142022.7vr20l.c19.kafka.us-east-1.amazonaws.com:2181,z-3.mpcs530142022.7vr20l.c19.kafka.us-east-1.amazonaws.com:2181 --replication-factor 1 --partitions 1 --topic tangn-public-rating
 
